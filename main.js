220040807cba5c1d44c8f6f924ea732e066e2223
Heera Singh Lodhi <heera9331@gmail.com>
2023-06-26 19:38:17 +0530

    promise1 solved

---
+++ main.js
@@ -0,0 +1,34 @@
+
+
+// function randomInRange(max, min) {
+//     return Math.floor(Math.random() * (max - min));
+// }
+
+// let myPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         if (randomInRange(1, 10) % 2 === 0) {
+//             resolve();
+//         } else {
+//             reject();
+//         }
+//     }, 2000);
+// }).then(() => {
+//     console.log('resolved');
+// }).catch(() => {
+//     console.log('rejected');
+// })
+
+
+let count = -10;
+let myPromise = new Promise((resolve, reject) => {
+    if (count > 0) {
+        resolve();
+    } else {
+        reject();
+    }
+}).then(() => {
+    console.log('resolved');
+}).catch(() => {
+    console.log('rejected');
+})
+
